9ca4e89ce79bca45bccbd4d7160564a440cb6c15
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-08-20 14:42:07 +0100

    complete setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-function component() {
-  const element = document.createElement('div');
+import _ from 'lodash';
+import './styles.css';
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+ function component() {
+   const element = document.createElement('div');
 
-  return element;
-}
+   // Lodash, now imported by this script
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
-document.body.appendChild(component());
+   return element;
+ }
+
+ document.body.appendChild(component());
